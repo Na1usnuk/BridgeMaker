9345511ac83fcccfe3cc18b9616faba39a6fd376
Na1usnuk <nazar1.bargan@gmail.com>
2025-12-30 18:55:40 +0300

    CPU side Image

--- impl/graphic/image.cpp
+++ impl/graphic/image.cpp
@@ -3,7 +3,7 @@ module;
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 
-module bm.core:image;
+module bm.gfx:image;
 
 import :image;
 
@@ -33,13 +33,13 @@ namespace bm
 		default: stbi_image_free(buff); throw ImageException("Unknown image pixel format", m_meta);
 		}
 
-		m_data.reset(buff);
+		m_data.reset(reinterpret_cast<std::byte*>(buff));
 		m_size = static_cast<std::size_t>(m_meta.width) * m_meta.height * bpp;
 	}
 
-	Image::Image(const std::span<const unsigned char> data, int width, int height, Format format) :
+	Image::Image(const std::span<const std::byte> data, int width, int height, Format format) :
 		m_meta{ width, height, format, "" },
-		m_data{ new unsigned char[data.size()], [](void* p) { delete[](p); } },
+		m_data{ new std::byte[data.size()], [](void* p) { delete[](p); } },
 		m_size(data.size())
 	{
 		if (format == Format::None) 
@@ -54,13 +54,11 @@ namespace bm
 	void Image::flipVertically() noexcept
 	{
 		auto data = getData();
-
 		const std::size_t row_size = static_cast<std::size_t>(m_meta.width) * getChannelsCount();
 
 		for (int y = 0; y < m_meta.height / 2; ++y)
 		{
 			auto row_top = data.subspan(static_cast<std::size_t>(y) * row_size, row_size);
-
 			auto row_bottom = data.subspan(static_cast<std::size_t>(m_meta.height - 1 - y) * row_size, row_size);
 
 			std::ranges::swap_ranges(row_top, row_bottom);
@@ -71,7 +69,6 @@ namespace bm
 	{
 		auto data = getData();
 		const int channels = getChannelsCount();
-
 		const std::size_t row_size = static_cast<std::size_t>(m_meta.width) * channels;
 
 		for (int y = 0; y < m_meta.height; ++y)
@@ -81,7 +78,6 @@ namespace bm
 			for (int x = 0; x < m_meta.width / 2; ++x)
 			{
 				auto lhs = row.subspan(static_cast<std::size_t>(x) * channels, channels);
-
 				auto rhs = row.subspan(static_cast<std::size_t>(m_meta.width - 1 - x) * channels,channels);
 
 				std::ranges::swap_ranges(lhs, rhs);
--- mod/graphic/image.cppm
+++ mod/graphic/image.cppm
@@ -1,6 +1,6 @@
-export module bm.core:image;
+export module bm.gfx:image;
 
-import :utility;
+import bm.core;
 
 import std;
 
@@ -19,7 +19,6 @@ namespace bm
 			RGBA = 4
 		};
 
-		// Metadata about the image
 		struct MetaData
 		{
 			int width = 0, height = 0;
@@ -30,7 +29,7 @@ namespace bm
 	public:
 
 		Image(const std::filesystem::path& filepath);
-		Image(const std::span<const unsigned char> data, int width, int height, Format format);
+		Image(const std::span<const std::byte> data, int width, int height, Format format);
 
 		Image() = delete;
 		Image(const Image&) = delete;
@@ -40,8 +39,8 @@ namespace bm
 		Image(Image&&) noexcept = default;
 		~Image() = default;
 
-		const std::span<const unsigned char> getData() const noexcept { return { m_data.get(), m_size}; }
-		std::span<unsigned char> getData() noexcept { return { m_data.get(), m_size }; }
+		const std::span<const std::byte> getData() const noexcept { return { m_data.get(), m_size}; }
+		std::span<std::byte> getData() noexcept { return { m_data.get(), m_size }; }
 		const std::filesystem::path& getFilepath() const noexcept { return m_meta.filepath; }
 		int getWidth() const noexcept { return m_meta.width; }
 		int getHeight() const noexcept { return m_meta.height; }
@@ -55,7 +54,7 @@ namespace bm
 	private:
 
 		MetaData m_meta;
-		std::unique_ptr<unsigned char, void(*)(void*)> m_data; // stbi_image_free or delete[]
+		std::unique_ptr<std::byte, void(*)(void*)> m_data; // stbi_image_free or delete[]
 		std::size_t m_size;
 	};
 
